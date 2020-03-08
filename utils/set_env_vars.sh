4420e852a73548152c1ad6b6c41600f59de73e34
honghuac <honghuac@gmail.com>
2020-03-08 19:02:29 +0800

    Add ERD ID to set env vars.sh file

--- utils/set_env_vars.sh
+++ utils/set_env_vars.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# ######     CHANGE THE VALUES IN THIS SECTION !!!!!!    ################ 
+# ######     CHANGE THE VALUES IN THIS SECTION !!!!!!    ################
+echo "export ERD_ID=<provided by instructor>" >> $HOME/.bashrc
 echo "export LAB_ID=<provided by instructor>" >> $HOME/.bashrc
 echo "export OCP_PASSWD=<provided by instructor>" >> $HOME/.bashrc
 echo "export LAB_MASTER_API=<provided by instructor>                   #   URL to OCP Master" >> $HOME/.bashrc
@@ -14,12 +15,12 @@ echo "export API_TENANT_PASSWORD=<provided by instructor>              #   Passw
 source $HOME/.bashrc
 
 
-echo "export ERDEMO_USER=user\$LAB_ID              # Emergency Response Demo user" >> $HOME/.bashrc
+echo "export ERDEMO_USER=user\$ERD_ID              # Emergency Response Demo user" >> $HOME/.bashrc
 echo "export ERDEMO_NS=\$ERDEMO_USER-er-demo      # Emergency Response namespace" >> $HOME/.bashrc
 echo "export SM_CP_ADMIN=admin\$LAB_ID             # Service Mesh control plan admin" >> $HOME/.bashrc
 echo "export SM_CP_NS=\$SM_CP_ADMIN-istio-system  # Service Mesh control plane namespace" >> $HOME/.bashrc
 
 echo "export API_MANAGER_NS=3scale-mt-\$OCP_AMP_ADMIN_ID      #  Namespace of 3scale API Mgmt control plane "   >> ~/.bashrc
-echo "export GW_PROJECT=\$ERDEMO_USER-gw                      #  Namespace of 3scale API gateways" >> ~/.bashrc
+echo "export GW_PROJECT=user\$LAB_ID-gw                      #  Namespace of 3scale API gateways" >> ~/.bashrc
 
 source $HOME/.bashrc
